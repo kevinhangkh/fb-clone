f7cd997aa3dd0fcdcd1a821d0e51dd3048b3c4c1
Kevin Hang <kevinhang.kh@gmail.com>
2021-04-19 12:05:17 +0200

    Changed access to a new Firestore Database. Previous one was compromised.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,20 @@
 export const environment = {
   production: true,
+  // firebaseConfig: {
+  //   apiKey: "AIzaSyCQPQx3T5lHRkeqP3_Ou3HiR5oBZy78nlg",
+  //   authDomain: "fb-clone-c17ed.firebaseapp.com",
+  //   databaseURL: "https://fb-clone-c17ed-default-rtdb.firebaseio.com",
+  //   projectId: "fb-clone-c17ed",
+  //   storageBucket: "fb-clone-c17ed.appspot.com",
+  //   messagingSenderId: "951596034535",
+  //   appId: "1:951596034535:web:d2023dbc0fd1e4dc2a7542"
+  // }
   firebaseConfig: {
-    apiKey: "AIzaSyCQPQx3T5lHRkeqP3_Ou3HiR5oBZy78nlg",
-    authDomain: "fb-clone-c17ed.firebaseapp.com",
-    databaseURL: "https://fb-clone-c17ed-default-rtdb.firebaseio.com",
-    projectId: "fb-clone-c17ed",
-    storageBucket: "fb-clone-c17ed.appspot.com",
-    messagingSenderId: "951596034535",
-    appId: "1:951596034535:web:d2023dbc0fd1e4dc2a7542"
+    apiKey: "AIzaSyC4n9h0Uaa8TNxvd39mavU1liRZhw1YjUM",
+    authDomain: "fakebook-b0165.firebaseapp.com",
+    projectId: "fakebook-b0165",
+    storageBucket: "fakebook-b0165.appspot.com",
+    messagingSenderId: "106395947696",
+    appId: "1:106395947696:web:0cffb75a3a32734007e175"
   }
 };
